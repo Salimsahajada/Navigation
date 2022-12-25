f4bdec6c5511fe60c6c9b5cffe492478d7ec8070
Salimsahajada <salimsahajada786@gmail.com>
2022-12-25 21:43:28 +0530

    navigation done

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// document.getElementById("inner").addEventListener("click",head);
+
+// function head(){
+
+//     document.getElementById("inner").parentElement.style.background = "pink";
+// }
+
+
+// document.getElementById("child-c").parentElement.style.background = "lightblue";
+// document.getElementById("inner").lastElementChild.style.background = "blue";
+
+// var a = document.getElementById("outer").parentElement;
+// var a = document.getElementById("child-c").parentNode;
+// var a = document.getElementById("inner").children;
+// var a = document.getElementById("inner").childNodes;
+// var a = document.getElementById("inner").firstElementChild;
+// var a = document.getElementById("inner").lastElementChild;
+// var a = document.getElementById("inner").firstChild;
+// var a = document.getElementById("inner").lastChild;
+var a = document.getElementById("outer").nextElementSibling;
+
+console.log(a);
